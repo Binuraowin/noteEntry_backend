91da2bb2137d592a56d67921bfff8a3d5cb2c70e
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-10-31 11:22:46 +0530

    change update note response

--- controllers/note_controller.js
+++ controllers/note_controller.js
@@ -4,48 +4,53 @@ const Note = require('../model/notes_model');
 
 exports.note_create = (req, res, next) => {
 
-          const note = new Note({
-            _id: new mongoose.Types.ObjectId(),
-            title:  req.body.title,
-            description:  req.body.description,
-          });
-          note.save()
-            .then(result => {
-              console.log(result);
-              res.status(201).json({
-                message: "Created note successfully",
-                data:result
-              });
-            })
-  }
-
-  exports.note_update = (req, res, next) => {
-    const id = req.params.id;
-    Note.findByIdAndUpdate(id,req.body,{ useFindAndModify: false })
-      .exec()
-      .then(result => {
-        console.log(result)
-        res.status(200).json({
-          id:result.id,
-          title: result.title,
-          description:  result.description,
-          visibility:  result.visibility,
-          date: result.date,         
-       });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
+  const note = new Note({
+    _id: new mongoose.Types.ObjectId(),
+    title: req.body.title,
+    description: req.body.description,
+  });
+  note.save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Created note successfully",
+        data: result
       });
-  }
+    })
+}
+
+exports.note_update = (req, res, next) => {
+  const id = req.params.id;
+  Note.findByIdAndUpdate(id, req.body, {
+      useFindAndModify: false
+    })
+    .exec()
+    .then(result => {
+      console.log(result)
+      res.status(200).json({
+        message: "updated successfully",
+        data: {
+          id: result.id,
+          title: result.title,
+          description: result.description,
+          visibility: result.visibility,
+          date: result.date,
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+}
 
 
-  exports.note_delete = (req, res, next) => {
-    const id = req.params.id;
-    console.log(id)
-    Note.find({
+exports.note_delete = (req, res, next) => {
+  const id = req.params.id;
+  console.log(id)
+  Note.find({
       _id: id,
     })
     .exec()
@@ -53,22 +58,22 @@ exports.note_create = (req, res, next) => {
       if (doc[0]) {
         console.log(doc)
         Note.findByIdAndRemove({
-          _id: doc[0]._id
-        })
-        .exec()
-        .then(result => {
-          res.status(202).json({
-            message: "Deleted note successfully",
-            data:result
+            _id: doc[0]._id
+          })
+          .exec()
+          .then(result => {
+            res.status(202).json({
+              message: "Deleted note successfully",
+              data: result
+            });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({
+              error: err
+            });
           });
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json({
-            error: err
-          });
-        });
-  
+
       } else {
         res
           .status(404)
@@ -83,5 +88,5 @@ exports.note_create = (req, res, next) => {
         error: err
       });
     });
-  
-  }
+
+}
